0950d7d6339070f16e53d73d89e1f51eb8075214
Mikko Ruskamo <hansgruge@users.noreply.github.com>
2016-10-04 03:00:40 +0300

    Made basic structure for parsing.

--- git/lemmadict.py
+++ git/lemmadict.py
@@ -1,5 +1,24 @@
 import gzip
+import re
+
+def read_data(file_to_read):
+    d = {}
+    with gzip.open(file_to_read, 'r') as rf:
+        for i, line in enumerate(rf):
+            if re.match('#', line):
+                continue
+            elif len(line) <= 1:
+                continue
+            else:
+                print line.decode('utf-8')
+            if i > 20:
+                break
+
+    return d
+
+def main():
+    read_data('../../versions/parsebank_v4_UD_scrambled.conllu-part-aa.gz')
 
 
-with gzip.open('../../versions/parsebank_v4_UD_scrambled.conllu-part-aa.gz', 'r') as rf:
-    print [(i, line) for i, line in enumerate(rf) if i < 10]
+if __name__ == '__main__':
+    main()
