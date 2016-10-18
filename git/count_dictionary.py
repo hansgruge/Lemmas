1801154677acf83b2d2abafe598d65093773e0f0
hansgruge <halli.kaukalo@gmail.com>
2016-10-18 09:56:11 +0300

    dict to dataframe

--- git/count_dictionary.py
+++ git/count_dictionary.py
@@ -1,31 +1,43 @@
 import sys
 import pickle
 
-d={}
+d = {}
 
-ID,FORM,LEMMA,UPOS,XPOS,FEAT,HEAD,DEPREL,DEPS,MISC=range(10)
+ID, FORM, LEMMA, UPOS, XPOS, FEAT, HEAD, DEPREL, DEPS, MISC = range(10)
 
 for line in sys.stdin:
-    line=line.rstrip()
+    line = line.rstrip()
     if not line or line.startswith("#"):
         continue
-    cols=line.split("\t")
-    assert len(cols)==10
-    word_dict=d.setdefault(cols[UPOS],{}).setdefault(cols[LEMMA],{})
-    lemma_reading=(cols[FORM],cols[FEAT])
-    word_dict[lemma_reading]=word_dict.get(lemma_reading,0)+1
+    cols = line.split("\t")
+    assert len(cols) == 10
+    word_dict = d.setdefault(cols[UPOS], {}).setdefault(cols[LEMMA], {})
+    lemma_reading = (cols[FORM], cols[FEAT])
+    word_dict[lemma_reading] = word_dict.get(lemma_reading, 0)+1
 
-with open("mydict.pickle","wb") as f:
-    pickle.dump(d,f,pickle.HIGHEST_PROTOCOL)
+import pandas as pd
+import matplotlib as mpl
 
-# pos_counts={}
-# for pos,pos_dict in d.items():
-#     pos_d=pos_counts.setdefault(pos,{})
-#     for word_dict in pos_dict.values():
-#         for (lemma,reading),count in word_dict.items():
-#             pos_d[reading]=pos_d.get(reading,0)+1
+df = pd.DataFrame([(k1,k2,k3,v) for k1, k23v in d.items()
+                  for k2, k3v in k23v.items()
+                  for k3, v in k3v.items()],
+                  columns=['POS', 'LEMMA', 'FORM + FEAT', 'pcs'])
+print(df.head())
 
-# for reading,count in sorted(pos_counts["ADJ"].items(),key=lambda x:x[1],reverse=True):
-#     print(reading," : ",count)
 
-    
+df['POS'].value_counts().plot(kind='bar')
+
+
+with open("mydict.pickle", "wb") as f:
+    pickle.dump(df, f, pickle.HIGHEST_PROTOCOL)
+
+#pos_counts = {}
+#for pos, pos_dict in d.items():
+#    pos_d = pos_counts.setdefault(pos, {})
+#    for word_dict in pos_dict.values():
+#        for (lemma, reading), count in word_dict.items():
+#            pos_d[reading] = pos_d.get(reading, 0)+1
+#
+#for reading, count in sorted(pos_counts["ADJ"].items(), key=lambda x: x[1],
+#                             reverse=True):
+#    print(reading, " : ", count)
